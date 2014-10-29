ccdc0945bd9d7f9da0c0edaff10e53add4c3d4ac
renderf0x <derek@barncard.com>
2014-10-29 15:33:01 -0700

    functional pattern happiness

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var firstItem = 0;
+  var lastItem = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[lastItem] = value;
+    lastItem++;
   };
 
   someInstance.dequeue = function(){
+    var temp = storage[firstItem];
+    delete storage[firstItem];
+    firstItem++;
+
+    return temp;
   };
 
   someInstance.size = function(){
+    return Object.keys(storage).length;
   };
 
   return someInstance;
