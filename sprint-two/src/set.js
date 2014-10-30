02581e0185c97c584b7e2dad2aa3e5484461cd10
renderf0x <derek@barncard.com>
2014-10-30 11:42:37 -0700

    in progress set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,50 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  //set._storage = undefined;
+  set.head = null;
+  set.tail = null;
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item){
+setPrototype.add = function(item){ //addToTail
+  if(this.set.head === null){
+    this.set.head = this.set.tail = this.makeNode(item);
+  } else {
+    var tempNode = this.set.tail;
+    this.set.tail = this.makeNode(item);
+    tempNode.next = this.set.tail;
+  }
 };
 
 setPrototype.contains = function(item){
+  var node = this.set.head;
+  while(node !== null){
+    if(node.value === item){
+      return true;
+    }
+    node = node.next;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  var node = this.set.head;
+  while(node !== null){
+    if (node.value === item){
+
+    }
+  }
 };
 
+setPrototype.makeNode = function(item){
+  var node = {
+    value : item,
+    next  :  null
+  };
+  return node;
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
