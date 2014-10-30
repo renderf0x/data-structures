921e61813128bd6388e09e588a1b0a4a95bab0e6
renderf0x <derek@barncard.com>
2014-10-29 22:29:35 -0700

    pseudoclassical pattern happy

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.firstItem = 0;
+  this.lastItem = 0;
+};
+
+Queue.prototype.size = function() {
+  return this.lastItem - this.firstItem;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.lastItem] = value;
+  this.lastItem++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.size() === 0)
+  {
+    return;
+  }
+  var temp = this.storage[this.firstItem];
+  delete this.storage[this.firstItem];
+  this.firstItem++;
+  return temp;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,39 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.counter = 0;
 };
 
 
+// Stack.prototype = {
+//   size: function(){
+//     return this.counter;
+//   },
+//   push: function(value){
+//     this.storage[this.counter] = value;
+//     this.counter++;
+//   },
+//   pop: function(){
+//     if (this.size() === 0){
+//       return;
+//     }
+//     this.counter--;
+//     return this.storage[this.counter];
+//   }
+// };
+
+Stack.prototype.size = function(){
+    return this.counter;
+  };
+Stack.prototype.push = function(value){
+    this.storage[this.counter] = value;
+    this.counter++;
+  };
+Stack.prototype.pop = function(){
+    if (this.size() === 0){
+      return;
+    }
+    this.counter--;
+    return this.storage[this.counter];
+  };
