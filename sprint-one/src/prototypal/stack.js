5da2d38f5ee3927823431ad32da6a000d6246a5d
renderf0x <derek@barncard.com>
2014-10-29 21:21:20 -0700

    prototypal patterns happy

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.storage = {};
+  obj.counter = 0;
+
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size : function(){
+    return this.counter;
+  },
+  push : function(value){
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+  pop : function(){
+    if (this.counter > 0)
+    {
+      this.counter--;
+      var temp = this.storage[this.counter];
+      delete this.storage[this.counter];
+      return temp;
+    }
+  }
+};
 
 
