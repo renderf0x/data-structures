6eede081c4d804042a7edf1c7a3548a1e3e6c05d
renderf0x <derek@barncard.com>
2014-10-30 21:04:29 -0700

    tree prompts finished

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,7 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -11,11 +12,30 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var childTree = makeTree(value);
+  if(this.children === undefined){
+    this.children = [];
+  }
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target){
+  var result = false;
 
+  var treeCurse = function(tree){
+    if(tree.value === target){
+      result = true;
+    }
+    if(!tree.children){
+      return;
+    }
+    for(var i = 0; i < tree.children.length; i++){
+      treeCurse(tree.children[i]);
+    }
+  };
+
+  treeCurse(this);
+  return result;
 };
 
 
